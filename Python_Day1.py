cd07816b4e437ac5fb6632a55b3ca295bdbcf37b
lokesh7382 <106101685+lokesh7382@users.noreply.github.com>
2022-05-23 23:05:12 +0530

    basics of Python and its DataTypes

---
+++ Python_Day1.py
@@ -0,0 +1,254 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+1+1 
+
+
+# In[3]:
+
+
+4*5
+
+
+# # python
+
+# In[6]:
+
+
+2*10
+
+
+# In[7]:
+
+
+2*'str'
+
+
+# In[8]:
+
+
+11/2
+
+
+# In[9]:
+
+
+int(11/2)
+
+
+# In[10]:
+
+
+11//2
+
+
+# In[11]:
+
+
+10**5
+
+
+# In[12]:
+
+
+11%5
+
+
+# In[15]:
+
+
+type(1),type('lokesh'),type(10.0)
+
+
+# In[16]:
+
+
+print(type(1),type('lokesh'),type(10.0))
+
+
+# In[17]:
+
+
+a=10
+type(a
+    )
+
+
+# In[19]:
+
+
+print("dhoni{}".format(a) )
+
+
+# In[20]:
+
+
+a=10
+
+
+# In[21]:
+
+
+len('lokesh')
+
+
+# In[30]:
+
+
+"Lokesh".isalnum()
+
+
+# In[31]:
+
+
+list=[]
+type(list)
+
+
+# In[32]:
+
+
+list=[10,20,22,30,34,55]
+
+
+# In[33]:
+
+
+len(list)
+
+
+# In[36]:
+
+
+list.append(30)
+
+
+# In[37]:
+
+
+list
+
+
+# In[38]:
+
+
+dir(list)
+
+
+# In[40]:
+
+
+list.clear()
+
+
+# In[42]:
+
+
+list
+
+
+# In[43]:
+
+
+list=[1,2,34,5,6,78,9,10,2]
+
+
+# In[44]:
+
+
+list.copy()
+
+
+# In[47]:
+
+
+list[2]
+
+
+# In[48]:
+
+
+list[:]
+
+
+# In[49]:
+
+
+list.index(1)
+
+
+# In[50]:
+
+
+list.insert(1,'Lokesh')
+
+
+# In[51]:
+
+
+list
+
+
+# In[55]:
+
+
+list.extend([5,3])
+
+
+# In[53]:
+
+
+list
+
+
+# In[57]:
+
+
+list.remove('Lokesh')
+sum(list)
+
+
+# In[58]:
+
+
+list.pop()
+
+
+# In[59]:
+
+
+list
+
+
+# In[60]:
+
+
+list.pop(2)
+
+
+# In[62]:
+
+
+list
+
+
+# In[ ]:
+
+
+list.count()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
